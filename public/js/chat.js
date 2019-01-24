4f5da6ecb7d11ab8e23b01891696ff4da0f4bfd1
Tolulope Popoola <tolulopeontop@gmail.com>
2019-01-24 23:11:54 +0100

    Server updated

---
+++ public/js/chat.js
@@ -0,0 +1,68 @@
+var socket = io();
+
+function scrollToBottom(){
+
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+       messages.scrollTop(scrollHeight);
+    }
+
+}
+
+        socket.on('connect', function () {
+         var params = jQuery.deparam(window.location.search);
+
+         socket.emit('join', params, function(err){
+                if(err){
+                    alert(err);
+                    window.location.href='/';
+                }else{
+                    console.log('No error')
+                }
+         })
+            
+        });
+
+        socket.on('disconnect', function (){
+            console.log('Disconnected from the server');
+        });
+
+        socket.on('updateUserList', function (users) {
+            console.log('Users list', users);
+        })
+
+        socket.on('newMessage', function (message) {
+            var formattedTime = moment(message.createdAt).format('h:mm a');
+            var template = jQuery('#message-template').html();
+            var html = Mustache.render(template, {
+                text: message.text,
+                from: message.from,
+                createdAt: formattedTime
+            });
+
+            jQuery('#messages').append(html);
+            scrollToBottom();
+        });
+
+    
+
+        jQuery('#message-form').on('submit', function(e){
+            e.preventDefault();
+
+            var messageTextbox = jQuery('[name=message]');
+
+            socket.emit('createMessage', {
+                from: 'User',
+                text: messageTextbox.val()
+            }, function () {
+               messageTextbox.val('')
+            });
+        });
