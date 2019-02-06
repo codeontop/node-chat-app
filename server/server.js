d0b0368903ae117582922cdf088f7fc3fba53823
Tolulope Popoola <tolulopeontop@gmail.com>
2019-02-06 17:49:03 +0100

    Send messages to only people in the same room

--- server/server.js
+++ server/server.js
@@ -39,8 +39,11 @@ io.on('connection', socket => {
     callback();
   });
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+    console.log(user);
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
     callback();
   });
 
